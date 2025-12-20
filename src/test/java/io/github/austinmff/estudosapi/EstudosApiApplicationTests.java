532c302d8cd6fa00300fbdcdedec35e877bfa4ef
Austinmff <austinmff@outlook.com>
2025-12-20 14:38:06 -0300

    Initial commit

---
+++ src/test/java/io/github/austinmff/estudosapi/EstudosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.austinmff.estudosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
