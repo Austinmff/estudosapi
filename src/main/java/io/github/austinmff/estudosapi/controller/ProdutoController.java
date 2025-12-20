6c71384ae3ad15561fd3a0f5f49968d24a7f7b5f
Austinmff <austinmff@outlook.com>
2025-12-20 16:36:58 -0300

    Implementa Porduto e ProdutoController para API

--- src/main/java/io/github/austinmff/estudosapi/controller/ProdutoController.java
+++ src/main/java/io/github/austinmff/estudosapi/controller/ProdutoController.java
@@ -1,4 +1,19 @@
 package io.github.austinmff.estudosapi.controller;
 
+import io.github.austinmff.estudosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
 public class ProdutoController {
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+        return produto;
+    }
+
 }
